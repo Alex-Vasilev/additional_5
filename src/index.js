39feafe8cab8185c4d69a755bb977e8779ee4123
Alex-Vasilev <aleksandr_vasilev1989@list.ru>
2017-09-17 14:34:20 +0300

    Solution: not finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+	let a = '';
+	bracketsConfig.forEach( i => {
+		var s = '\\' + i.join('\\');
+		a= a + '|' + s;
+	});
+
+	a = a.slice(1);
+	const regEx = new RegExp( a ,'g');
+	console.log(regEx);
+
+	while (regEx.exec(str)) { 
+		str = str.replace(regEx, '');
+	}
+	return !str.length;
 }
